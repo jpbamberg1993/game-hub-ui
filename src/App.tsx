aeb03c04adeb087763c3fc1ad005e3821cded4c3
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-05-14 13:08:01 -0500

    Fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import React from "react";
 import { NavBar } from "./components/nav-bar";
+import { GameGrid } from "./components/game-grid"
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
       <Show above="lg">
         <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main">Main</GridItem>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
     </Grid>
   )
 }
