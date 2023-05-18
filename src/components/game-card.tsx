bf690793e26c7c7e26699f7dba9d9f67f41a21b0
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-05-17 19:25:41 -0500

    Display platform icons

--- src/components/game-card.tsx
+++ src/components/game-card.tsx
@@ -1,16 +1,18 @@
 import { Game } from "../hooks/use-games"
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react"
+import { PlatformIconList } from "./platform-icon-list"
 
 interface Props {
 	game: Game;
 }
 
-export function GameCard({ game }: Props) {
+export function GameCard({game}: Props) {
 	return (
 		<Card borderRadius={10} overflow="hidden">
 			<Image src={game.background_image} alt={game.name} />
 			<CardBody>
 				<Heading fontSize="2xl">{game.name}</Heading>
+				<PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
 			</CardBody>
 		</Card>
 	)
---
+++ src/components/platform-icon-list.tsx
@@ -0,0 +1,46 @@
+import { FaAndroid, FaApple, FaLinux, FaPlaystation, FaWindows, FaXbox } from "react-icons/fa"
+import { MdPhoneIphone } from "react-icons/md"
+import { SiNintendo } from "react-icons/si"
+import { BsGlobe } from "react-icons/bs"
+import { Platform } from "../hooks/use-games"
+import { IconType } from "react-icons"
+import { Box, BoxProps, HStack, Icon } from "@chakra-ui/react"
+
+interface Props {
+	platforms: Platform[]
+}
+
+type IconProps = {
+	icon: IconType
+} & BoxProps
+
+function IconWrapper({icon: Icon, ...props}: IconProps) {
+	return (
+		<Box {...props}>
+			<Icon />
+		</Box>
+	)
+}
+
+export function PlatformIconList({platforms}: Props) {
+	const iconMap = new Map<string, IconType>([
+		['pc', FaWindows],
+		['xbox', FaXbox],
+		['playstation', FaPlaystation],
+		['nintendo', SiNintendo],
+		['mac', FaApple],
+		['linux', FaLinux],
+		['ios', MdPhoneIphone],
+		['android', FaAndroid],
+		['web', BsGlobe],
+	])
+
+	return (
+		<HStack marginY={1}>
+			{platforms.map(platform => {
+				const IconComponent = iconMap.get(platform.slug)
+				return IconComponent ? <IconWrapper icon={IconComponent} key={platform.id} color='gray.500' /> : null
+			})}
+		</HStack>
+	)
+}
