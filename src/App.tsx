459f4e98ecaf066fc4ca77c60a4706fb79c69f59
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-05-20 13:38:39 -0500

    Highlight selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onGenreSelect={setSelectedGenre} />
+          <GenreList selectedGenre={selectedGenre} onGenreSelect={setSelectedGenre} />
         </GridItem>
       </Show>
       <GridItem area="main">
--- src/components/genre-list.tsx
+++ src/components/genre-list.tsx
@@ -1,12 +1,13 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react"
+import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react"
 import { Genre, useGenres } from "../hooks/use-genres"
 import { getCroppedImageUrl } from "../services/image-url"
 
 interface Props {
 	onGenreSelect: (genre: Genre) => void
+	selectedGenre: Genre | null
 }
 
-export function GenreList({ onGenreSelect }: Props) {
+export function GenreList({ selectedGenre, onGenreSelect }: Props) {
 	const {data, isLoading, error} = useGenres()
 
 	if (error) return null
@@ -15,20 +16,30 @@ export function GenreList({ onGenreSelect }: Props) {
 
 	return (
 		<List>
-			{data.map(genre => (
-				<ListItem key={genre.id} paddingY="5px">
-					<HStack>
-						<Image 
-							src={getCroppedImageUrl(genre.image_background)} 
-							alt={genre.name} 
-							boxSize='32px' 
-							borderRadius={8} />
-						<Button onClick={() => onGenreSelect(genre)} fontSize='lg' variant='link'>
-							{genre.name}
-						</Button>
-					</HStack>
-				</ListItem>
-			))}
+			{data.map(genre => {
+				const isSelected = selectedGenre?.id === genre.id
+
+				return (
+					<ListItem 
+						key={genre.id} 
+						paddingY="5px">
+						<HStack>
+							<Image 
+								src={getCroppedImageUrl(genre.image_background)} 
+								alt={genre.name} 
+								boxSize='32px' 
+								borderRadius={8} />
+							<Button 
+								onClick={() => onGenreSelect(genre)} 
+								fontWeight={isSelected ? 'bold' : 'normal'} 
+								fontSize='lg' 
+								variant='link'>
+								{genre.name}
+							</Button>
+						</HStack>
+					</ListItem>
+				)
+			})}
 		</List>
 	)
 }
