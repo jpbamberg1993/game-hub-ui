75c05ad076ca732a791d461d9777add3853c8b8d
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-05-24 07:27:02 -0500

    Cleanup genre list

--- src/components/genre-list.tsx
+++ src/components/genre-list.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react"
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from "@chakra-ui/react"
 import { Genre, useGenres } from "../hooks/use-genres"
 import { getCroppedImageUrl } from "../services/image-url"
 import { Nullable } from "../types/utility-types"
@@ -16,31 +16,39 @@ export function GenreList({selectedGenre, onGenreSelect}: Props) {
 	if (isLoading) return <Spinner />
 
 	return (
-		<List>
-			{data.map(genre => {
-				const isSelected = selectedGenre?.id === genre.id
+		<>
+			<Heading fontSize='2xl' marginBottom={3} marginTop={1}>
+				Genres
+			</Heading>
+			<List>
+				{data.map(genre => {
+					const isSelected = selectedGenre?.id === genre.id
 
-				return (
-					<ListItem
-						key={genre.id}
-						paddingY="5px">
-						<HStack>
-							<Image
-								src={getCroppedImageUrl(genre.image_background)}
-								alt={genre.name}
-								boxSize="32px"
-								borderRadius={8} />
-							<Button
-								onClick={() => onGenreSelect(genre)}
-								fontWeight={isSelected ? 'bold' : 'normal'}
-								fontSize="lg"
-								variant="link">
-								{genre.name}
-							</Button>
-						</HStack>
-					</ListItem>
-				)
-			})}
-		</List>
+					return (
+						<ListItem
+							key={genre.id}
+							paddingY="5px">
+							<HStack>
+								<Image
+									src={getCroppedImageUrl(genre.image_background)}
+									alt={genre.name}
+									boxSize="32px"
+									borderRadius={8}
+									objectFit='cover' />
+								<Button
+									onClick={() => onGenreSelect(genre)}
+									fontWeight={isSelected ? 'bold' : 'normal'}
+									fontSize="lg"
+									whiteSpace='normal'
+									textAlign='left'
+									variant="link">
+									{genre.name}
+								</Button>
+							</HStack>
+						</ListItem>
+					)
+				})}
+			</List>
+		</>
 	)
 }
