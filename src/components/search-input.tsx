bb3f67f4cf94f214d7898417e5328d55e1b848b9
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-08-01 06:49:45 -0400

    Remove unusaed reference

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,7 +1,6 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
-import { Nullable } from "../types/utility-types"
 
 interface Props {
 	onSearch: (searchText: string) => void
