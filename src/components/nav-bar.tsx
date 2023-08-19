31095777e0d7fcc1d6f62a084fb9577b41586213
Paul Bamberg <jpbamberg1993@gmail.com>
2023-08-19 15:37:48 -0500

    Fix nav bar functionality

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react"
 import logo from "../assets/logo.webp"
 import { ColorModeSwitch } from "./color-mode-switch"
 import { SearchInput } from "./search-input"
+import { NavLink } from "react-router-dom"
 
 export function NavBar() {
 	return (
 		<HStack padding="0 10px 0">
-			<Image src={logo} alt="logo" boxSize="60px" />
+			<NavLink to="/">
+				<Image src={logo} alt="logo" boxSize="60px" objectFit="cover" />
+			</NavLink>
 			<SearchInput />
 			<ColorModeSwitch />
 		</HStack>
--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -2,16 +2,19 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import { useGameStore } from "../stores/game-store"
+import { useNavigate } from "react-router-dom"
 
 export function SearchInput() {
 	const ref = useRef<HTMLInputElement | null>(null)
 	const setSearchText = useGameStore(s => s.setSearchText)
+	const navigate = useNavigate()
 
   return (
 		<form onSubmit={(event) => {
 			event.preventDefault()
 			if (ref.current) {
 				setSearchText(ref.current.value)
+				navigate('/')
 			}
 		}}>
 			<InputGroup>
